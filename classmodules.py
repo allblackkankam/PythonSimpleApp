6f29078805dbf4cd1014f77883a3ba9cb516cbdb
allblackkankam <kankamsolomon@gmail.com>
2017-06-17 15:39:51 +0100

    startup python code

---
+++ classmodules.py
@@ -0,0 +1,5 @@
+import subprocess
+
+subprocess.call("C:\Program Files\Adobe\Adobe Photoshop CC 2015\photoshop");
+subprocess.call("C:\Program Files\VideoLAN\VLC\Vlc");
+subprocess.call("C:\Program Files (x86)\Mozilla Firefox\Firefox");

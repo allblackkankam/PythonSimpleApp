f4273053bb952c496b32681a312b09a84118c21f
Solomon Kankam <kankamsolomon@gmail.com>
2017-06-25 16:56:03 +0000

    Delete classmodules.py

--- classmodules.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-subprocess.call("C:\Program Files\Adobe\Adobe Photoshop CC 2015\photoshop");
-subprocess.call("C:\Program Files\VideoLAN\VLC\Vlc");
-subprocess.call("C:\Program Files (x86)\Mozilla Firefox\Firefox");

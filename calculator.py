de26e64909b9eacf699e02903fb542f9fa0bfc09
Solomon Kankam <kankamsolomon@gmail.com>
2017-06-28 18:21:36 +0000

    Delete calculator.py

--- calculator.py
+++
@@ -1,8 +0,0 @@
-print"SIMPLE CALCULATOR"
-print"Use Add = +,Subtract= -,Divide= /,Multipy= *,0=Exit"
-Number=raw_input("Enter Numbers: ")
-# Numbers1=raw_input("Enter Numbers: ")
-def add(n,p):
-	print n+p
-
-add (Numbers	,6)
